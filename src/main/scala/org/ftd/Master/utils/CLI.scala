116cd4a2d8113b751a1ee9d4ca94f5fad2479739
Xiao Liang <59068619+xiaoly9@users.noreply.github.com>
2021-08-23 22:48:15 +0800

    feat(CLI): auto find adb

--- src/main/scala/org/ftd/Master/utils/CLI.scala
+++ src/main/scala/org/ftd/Master/utils/CLI.scala
@@ -1,6 +1,7 @@
 package org.ftd.Master.utils
 
 import java.io.File
+import java.io.FileNotFoundException
 import java.lang.Thread.UncaughtExceptionHandler
 import java.nio.file.{Files, Paths}
 
@@ -23,82 +24,82 @@ object CLI {
       }
     })
 
-    import scopt.OParser
+    import scopt.OptionParser
 
-    val builder = OParser.builder[Config]
-    val parser = {
-      import builder._
-      OParser.sequence(
-        programName("FTD"),
+    val parser = new OptionParser[Config]("FlakeScanner") {
 
         opt[String]("adbPath")
           .required()
-          // .withFallback(() => System.getenv("PATH").split(File.pathSeparator).map(p => Paths.get(p).resolve("adb")).find( p => Files.exists(p) ).head )
-          .action((x, c) => c.copy(adbPath = Paths get x)),
+          .withFallback(() => 
+                        ( System.getenv("PATH").split(File.pathSeparator)
+                          .map(p => Paths.get(p).resolve("adb")).find( p => Files.exists(p) ) ) match {
+                            case Some(path) => path.toString()
+                            case None => throw new FileNotFoundException("ADB not found")
+                          }
+                       )
+          .action((x, c) => c.copy(adbPath = Paths get x))
 
         opt[Unit]("debug")
-          .action((_, c) => c.copy(debug = true)),
+          .action((_, c) => c.copy(debug = true))
 
         opt[Unit]("disable-ddm-log")
-          .action((_, c) => c.copy(disable_ddm_log = true)),
+          .action((_, c) => c.copy(disable_ddm_log = true))
 
         opt[Seq[String]]("apkInstallOpts")
           .optional()
-          .action((x, c) => c.copy(apkInstallOpts = x)),
+          .action((x, c) => c.copy(apkInstallOpts = x))
 
         opt[String]("deviceName")
           .optional()
           .action((x, c) => c.copy(deviceName = x))
-          .text("Use default device if not supplied"),
+          .text("Use default device if not supplied")
 
         opt[Int]("max-runs")
           .optional()
-          .action((x, c) => c.copy(max_runs = x)),
+          .action((x, c) => c.copy(max_runs = x))
 
         opt[File]("config-from-file")
             .optional()
-            .action((x, c)=>c.copy(from_files = x)),
+            .action((x, c)=>c.copy(from_files = x))
 
         opt[Duration]("test-hang-timeout")
             .optional()
-            .action((x, c)=>c.copy(testCaseHangTimeout = x)),
+            .action((x, c)=>c.copy(testCaseHangTimeout = x))
 
         opt[Boolean]("given-passed")
           .optional()
-          .action((x, c)=>c.copy(givenPassed = x)),
+          .action((x, c)=>c.copy(givenPassed = x))
 
         opt[String]("strategy")
             .optional()
-            .action((x, c)=>c.copy(strategy = x)),
+            .action((x, c)=>c.copy(strategy = x))
 
         arg[String]("appName")
           .required()
-          .action((x, c) => c.copy(appName = x)),
+          .action((x, c) => c.copy(appName = x))
 
         arg[String]("testPackage")
           .required()
-          .action((x, c) => c.copy(testPackage = x)),
+          .action((x, c) => c.copy(testPackage = x))
 
         arg[Seq[String]]("apkPath")
           .required()
-          .action((x, c) => c.copy(apkPath = x.map(p => Paths.get(p)))),
+          .action((x, c) => c.copy(apkPath = x.map(p => Paths.get(p))))
 
         arg[String]("testRunnerClsPath")
           .required()
-          .action((x, c) => c.copy(testRunnerClsPath = x)),
+          .action((x, c) => c.copy(testRunnerClsPath = x))
 
         arg[String]("testClassPath")
           .required()
-          .action((x, c) => c.copy(testClassPath = x)),
+          .action((x, c) => c.copy(testClassPath = x))
 
         arg[String]("testMethodPath")
           .required()
-          .action((x, c) => c.copy(testMethodPath = x)),
-
-      )
+          .action((x, c) => c.copy(testMethodPath = x))
     }
 
-    OParser.parse(parser, args, Config()) match {
+    parser.parse(args, Config()) match {
       case Some(config) => Main.main(config)
       case None => // Do nothing
     }
